7b6a7f5ad31eaf6e9133a553a1221e698c56edab
galvin-SH <mgalvin1993@gmail.com>
2023-09-14 16:55:10 -0700

    finalizing function definitions and calls in index.js

--- index.js
+++ index.js
@@ -1,82 +1,90 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs');
 const gm = require('./generateMarkdown');
 
-
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        name: 'title',
-        type: 'input',
-        message: 'Title:'
-    },
-    {
-        name: 'desc',
-        type: 'input',
-        message: 'Description:'
-    },
-    {
-        name: 'install',
-        type: 'input',
-        message: 'Installation Instructions:'
-    },
-    {
-        name: 'usage',
-        type: 'input',
-        message: 'Usage Information:'
-    },
-    {
-        name: 'contribute',
-        type: 'input',
-        message: 'Contribution Guidelines:'
-    },
-    {
-        name: 'test',
-        type: 'input',
-        message: 'Test Instructions:'
-    },
-    {
-        name: 'license',
-        type: 'list',
-        message: 'Select which license to include:',
-        choices: [
-            'Apache 2.0',
-            'GNU v3.0',
-            'MIT',
-            'BSD 2-Clause',
-            'BSD 3-Clause',
-            'Boost Software 1.0',
-            'Creative Commons Zero v1.0',
-            'Eclipse Public 2.0',
-            'GNU Affero General Public v3.0',
-            'GNU General Public v2.0',
-            'GNU Lesser General Public v2.1',
-            'Mozilla Public 2.0',
-            'The Unlicense']
-    },
-    {
-        name: 'username',
-        type: 'input',
-        message: 'GitHub Username:'
-    },
-    {
-        name: 'email',
-        type: 'input',
-        message: 'Contact Email:'
-    }
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        console.log(JSON.stringify(answers, null, '  '));
+let questions;
+// Retireve the names of commonly used licenses, then calls the initialize function which accepts the license names
+// Finally initiates the prompt once setup is complete
+function prepareLicenseNames() {
+    const licenseNames = [];
+    fetch(`https://api.github.com/licenses`)
+    .then(function(response){
+        return response.json();
     })
-};
-
-// Function call to initialize app
-init();
+    .then(function(data){
+        data.forEach(element => {
+            licenseNames.push(element.name)
+        });;
+        initializeQuestions(licenseNames)
+        startPrompt(questions)
+    })
+}
+// Initialize the questions object for use in the inquirer prompt method
+function initializeQuestions(choices){
+    questions = [
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Title:'
+        },
+        {
+            name: 'desc',
+            type: 'input',
+            message: 'Description:'
+        },
+        {
+            name: 'install',
+            type: 'input',
+            message: 'Installation Instructions:'
+        },
+        {
+            name: 'usage',
+            type: 'input',
+            message: 'Usage Information:'
+        },
+        {
+            name: 'contribute',
+            type: 'input',
+            message: 'Contribution Guidelines:'
+        },
+        {
+            name: 'test',
+            type: 'input',
+            message: 'Test Instructions:'
+        },
+        {
+            name: 'license',
+            type: 'list',
+            message: 'Select which license to include:',
+            choices: choices
+        },
+        {
+            name: 'username',
+            type: 'input',
+            message: 'GitHub Username:'
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'Contact Email:'
+        }
+    ];
+}
+// Starts the prompt when called and handles the answers object when complete
+function startPrompt(questions) {
+    inquirer.prompt(questions)
+        .then((answers) => {
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log(error);
+            } else {
+                console.error('Something went wrong', error);
+            }
+        })
+}
+prepareLicenseNames()
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    fs.writeFile(`README.md`, data,)
+}
